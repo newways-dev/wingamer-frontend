99c038df914570b554c2afe2b59b313e51924dfe
Kate <kate.dew.next@gmail.com>
2026-09-19 22:23:12 +0200

    Remove unused imports and dead commented-out code causing CI build failures

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,17 +1,13 @@
-import clsx from 'clsx'
 import { Link, useLocation } from 'react-router-dom'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { ReactSVG } from 'react-svg'
 import { useSelector, useDispatch } from 'react-redux'
 
-import { Button, Language, Logo, Profile } from '../../components'
-import { links } from '../../helpers/links'
+import { Language, Logo, Profile } from '../../components'
 import styles from './Header.module.scss'
 
-import { selectColor } from '../../redux/color/selector'
 import { setOpen as setMenuOpen } from '../../redux/menu/slice'
 import { setColor } from '../../redux/color/slice'
-import { setOpen } from '../../redux/search/slice'
 import { setOpenModal, setType } from '../../redux/modal/slice'
 
 import image from '../../assets/images/profile-image.png'
@@ -23,7 +19,6 @@ import { Links } from '../../components/Links/Links'
 export const Header = () => {
   const dispatch = useDispatch()
   const { pathname } = useLocation()
-  const { color } = useSelector(selectColor)
   const { openMenu } = useSelector(selectMenu)
 
   useEffect(() => {
@@ -41,11 +36,6 @@ export const Header = () => {
     dispatch(setOpenModal(true))
   }
 
-  const handleLogin = () => {
-    dispatch(setType('login'))
-    dispatch(setOpenModal(true))
-  }
-
   return (
     <header className={styles.header}>
       <div className={styles.wrapper}>
@@ -57,17 +47,6 @@ export const Header = () => {
             className={styles.search}
             src={search}
           />
-          {/* <button
-            className={clsx(styles.login, {
-              [styles.loginBlue]: color === 'blue',
-              [styles.loginOrange]: color === 'orange',
-              [styles.loginGreen]: color === 'green',
-              [styles.loginPurple]: color === 'purple',
-            })}
-            onClick={() => handleLogin()}
-          >
-            Login
-          </button> */}
           <Link to="/account">
             <Profile userName="DenisVykes" coins="11.58 coins" image={image} />
           </Link>
--- src/page-components/Intro/Main/Squares.tsx
+++ src/page-components/Intro/Main/Squares.tsx
@@ -1,80 +1,9 @@
-import clsx from 'clsx'
-import { Square } from './Square/Square'
-import styles from '../Intro.module.scss'
-
 import image from '../../../assets/images/home-banner.png'
-
-// const firstRow = [
-//   <Square className={clsx(styles.square, styles.transparent)} />,
-//   <Square className={clsx(styles.square, styles.orange)} />,
-//   <Square className={clsx(styles.square, styles.orange)} />,
-//   <Square className={clsx(styles.square, styles.green)} />,
-//   <Square className={clsx(styles.square, styles.green)} />,
-//   <Square className={clsx(styles.square, styles.green)} />,
-//   <Square className={clsx(styles.square, styles.green)} />,
-//   <Square className={clsx(styles.square, styles.purple)} />,
-// ]
-
-// const secondRow = [
-//   <Square className={clsx(styles.square, styles.orange)} />,
-//   <Square className={clsx(styles.square, styles.orange)} />,
-//   <Square className={clsx(styles.square, styles.purple)} />,
-//   <Square className={clsx(styles.square, styles.purple)} />,
-//   <Square className={clsx(styles.square, styles.purple)} />,
-//   <Square className={clsx(styles.square, styles.orange)} />,
-//   <Square className={clsx(styles.square, styles.orange)} />,
-//   <Square className={clsx(styles.square, styles.purple)} />,
-// ]
-// const thirdRow = [
-//   <Square className={clsx(styles.square, styles.transparent)} />,
-//   <Square className={clsx(styles.square, styles.blue)} />,
-//   <Square className={clsx(styles.square, styles.purple)} />,
-//   <Square className={clsx(styles.square, styles.green)} />,
-//   <Square className={clsx(styles.square, styles.green)} />,
-//   <Square className={clsx(styles.square, styles.orange)} />,
-//   <Square className={clsx(styles.square, styles.orange)} />,
-//   <Square className={clsx(styles.square, styles.purple)} />,
-// ]
-// const fourthRow = [
-//   <Square className={clsx(styles.square, styles.transparent)} />,
-//   <Square className={clsx(styles.square, styles.blue)} />,
-//   <Square className={clsx(styles.square, styles.blue)} />,
-//   <Square className={clsx(styles.square, styles.blue)} />,
-//   <Square className={clsx(styles.square, styles.green)} />,
-//   <Square className={clsx(styles.square, styles.green)} />,
-//   <Square className={clsx(styles.square, styles.blue)} />,
-//   <Square className={clsx(styles.square, styles.purple)} />,
-// ]
-// const fifthRow = [
-//   <Square className={clsx(styles.square, styles.transparent)} />,
-//   <Square className={clsx(styles.square, styles.orange)} />,
-//   <Square className={clsx(styles.square, styles.orange)} />,
-//   <Square className={clsx(styles.square, styles.orange)} />,
-//   <Square className={clsx(styles.square, styles.orange)} />,
-//   <Square className={clsx(styles.square, styles.blue)} />,
-//   <Square className={clsx(styles.square, styles.blue)} />,
-//   <Square className={clsx(styles.square, styles.blue)} />,
-// ]
 
 export const Squares = () => {
   return (
     <>
       <img src={image} alt="" />
-      {/* {firstRow.map((item, index) => (
-        <div key={index}>{item}</div>
-      ))}
-      {secondRow.map((item, index) => (
-        <div key={index}>{item}</div>
-      ))}
-      {thirdRow.map((item, index) => (
-        <div key={index}>{item}</div>
-      ))}
-      {fourthRow.map((item, index) => (
-        <div key={index}>{item}</div>
-      ))}
-      {fifthRow.map((item, index) => (
-        <div key={index}>{item}</div>
-      ))} */}
     </>
   )
 }
